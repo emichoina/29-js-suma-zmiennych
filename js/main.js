d00d9ffa9c4a556c967c1c42ea2767d1791eb52a
emi <emichoina@gmail.com>
2021-04-05 20:39:08 +0200

    29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 5;
+let secondNumber = 10;
+
+function add(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+
+    return result;
+}
+
+let suma = add(firstNumber, secondNumber);
+
+console.log(suma);
